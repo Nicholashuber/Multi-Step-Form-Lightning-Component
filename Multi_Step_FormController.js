5c07be19664014837a1508c320b7c2acd90da915
Nicholas Huber <Nick@huber.dev>
2020-07-24 01:57:56 -0400

    Update Multi_Step_FormController.js

--- Multi_Step_FormController.js
+++ Multi_Step_FormController.js
@@ -10,15 +10,35 @@
     },
     goToStepThree : function(component, event, helper) {
         helper.toggleThreeAndFourSteps(component);
-        component.set("v.progressIndicatorFlag", "step3");
+        component.set("v.progressIndicatorFlag", "step3"); 
     },
     goToStepFour : function(component, event, helper) {
         helper.toggleFourAndFiveSteps(component);
-        component.set("v.progressIndicatorFlag", "step4");
+        component.set("v.progressIndicatorFlag", "step4"); 
     },
     goToStepFive : function(component, event, helper) {
         helper.toggleFiveAndSixSteps(component);
-        component.set("v.progressIndicatorFlag", "step5");
+		component.set("v.progressIndicatorFlag", "step5");
+        
+        /*var action = component.get('c.Journal_Entry');
+        var feeloption214 = event.target.value;
+        action.setParams({
+           "eventtype":"Journal"
+       });
+       action.setCallback(this, function(response) {
+        //store state of response
+        var state = response.getState();
+        let errors = response.getError();
+        let message = 'Unknown error'; // Default error message
+        if (state === "SUCCESS") {
+         //set response value in ListOfAccount attribute on component.
+         console.log('SUCCESS2');
+        } else{
+            console.log('Error');
+            console.log('Error'+errors[0].message);
+        }
+       });
+       $A.enqueueAction(action);*/
     },
     goBackToStepOne : function(component, event, helper) {
         helper.toggleOneAndTwoSteps(component);
